e166f8d113c9e09fe7c9d59ad6945e0aebafc5e1
CalliopeVanrogue <harleyquinn771@gmail.com>
2024-11-03 23:39:06 -0800

    Game Complete

---
+++ Assets/Scenes/Scripts/GameCritic.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCritic : MonoBehaviour
+{
+//I felt the game was too hard but also too easy. By that I mean I found it almost impossible to actually hit the balls but after about 3 minutes the balls themselves managed to eventually go into the goals so I won the game regardless
+// Adjusting the player speed might help. Perhaps being able to rotate the bumper might work as well.
+// Making the balls different sizes would make it a bit more appealing and fun to navigate. I think that it would also be pretty cool if the balls and the bumper moved more than horizontally like maybe they flew around and so did you as a player
+//Personally I didn't find the game fun at all. I was not able to really hit the balls at all and the game just kind of solved itself. Running around was pretty tedious honestly
+
+//To change the game I changed the shape and speed of the bumper
+//My second change was to make the bumper a bit more complex by changing the shape of the bumper
+//My third change was adding variations of the chaos balls. I added more as well as different sizes so it's slightly more chaotic
+//The fourth and final change and my favorite is I added pillars to the scene that have the bouncy material assigned as well to make it even harder to win the game
+}
